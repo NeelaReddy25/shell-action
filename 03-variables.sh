2a89b7a74def3f88f5f09b86b1ef8b34366ae0ab
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-17 12:56:43 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=RAMESH # No space between = and value
-PERSON2=SURESH
+PERSON1=SACHIN # No space between = and value
+PERSON2=RAHUL
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
